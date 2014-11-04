16c3206f5c36631c602e8a8616970ab25683256e
pnikven <pyshkinnv@gmail.com>
2014-11-04 18:13:14 +0500

    strong tag processing added

--- MarkdownProcessor/MarkdownProcessor.cs
+++ MarkdownProcessor/MarkdownProcessor.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Text;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 
@@ -17,12 +19,22 @@ namespace MarkdownProcessor
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("File name must eb provided");
+                Console.WriteLine("File name must be provided");
                 return;
             }
             
             var filename = args[0];
 
+            var filecontent = "";
+            using (var sr = new StreamReader(filename, Encoding.UTF8))
+                filecontent = sr.ReadToEnd();
+
+            var result = string.Join(PSeparator, 
+                ExtractParagraphs(filecontent)
+                .Select(p => WrapStrong(WrapEm(p))));
+
+            using (var sw = new System.IO.StreamWriter(filename+".html", false, Encoding.UTF8))
+                sw.Write(result);
 
         }
 
@@ -41,14 +53,28 @@ namespace MarkdownProcessor
         {
             return Regex.Split(input, PSeparatorPattern, RegexOptions.Singleline)
                 .Where(st => st != "")
-                .Select(st => st.StartsWith(POpen) ? st : POpen + st)
-                .Select(st => st.EndsWith(PClose) ? st : st + PClose)
+                .Select(EscapeAngleBrackets)
+                .Select(p => "<p>"+p+"</p>")
                 .ToArray();
+        }
+
+        private static string EscapeAngleBrackets(string st)
+        {
+            return st.Replace("<","&lt;").Replace(">","&gt;");
         }
 
         public static string WrapEm(string input)
         {
-            var result = Regex.Replace(input, "(?<!_)_([^_]+)_(?!_)", "<em>$1</em>");
+            var result = Regex.Replace(input,
+                @"(?<![\\_])_(" + @"(_{2,}|[^_])+" + @")(?<!\\)_(?!_)", "<em>$1</em>");
+
+            return result;
+        }
+
+        public static string WrapStrong(string input)
+        {
+            var result = Regex.Replace(input,
+                @"(?<![\\_])__(?!_)(((?!__).)*)(?<![\\_])__(?!_)", "<strong>$1</strong>");
 
             return result;
         }
@@ -89,13 +115,13 @@ namespace MarkdownProcessor
         }
 
         [Test]
-        public void leave_one_paragraph_unchanged()
+        public void escape_html_tags()
         {
             var input = "<p>One sentence.</p>";
 
             var result = MarkdownProcessor.WrapDoubleNewlinesToParagraphs(input);
 
-            Assert.AreEqual("<p>One sentence.</p>", result);
+            Assert.AreEqual("<p>&lt;p&gt;One sentence.&lt;/p&gt;</p>", result);
         }
 
         [Test]
@@ -109,9 +135,9 @@ namespace MarkdownProcessor
         }
 
         [Test]
-        public void leave_two_paragraphs_unchanged()
+        public void join_two_paragraphs_by_PSeparator()
         {
-            var input = "<p>One sentence.</p>\n    \n<p>Two sentence</p>";
+            var input = "One sentence.\n    \nTwo sentence";
 
             var result = MarkdownProcessor.WrapDoubleNewlinesToParagraphs(input);
 
@@ -120,17 +146,7 @@ namespace MarkdownProcessor
         }
 
         [Test]
-        public void wrap_sentence_and_paragraph_to_two_paragraphs()
-        {
-            var input = "<p>One sentence.</p>\n    \nTwo sentence";
-
-            var result = MarkdownProcessor.WrapDoubleNewlinesToParagraphs(input);
-
-            Assert.AreEqual("<p>One sentence.</p>" + MarkdownProcessor.PSeparator + "<p>Two sentence</p>", result);
-        }
-
-        [Test]
-        public void wrap_text_between_two_inderscores_to_em()
+        public void wrap_text_between_two_underscores_to_em()
         {
             var input = "Текст _окруженный с двух сторон_  одинарными символами подчерка";
 
@@ -140,15 +156,174 @@ namespace MarkdownProcessor
         }
 
         [Test]
-        public void not_wrap_text_between_two_double_inderscores_to_em()
+        public void not_wrap_text_between_two_double_underscores_to_em()
         {
             var input = "Текст __окруженный с двух сторон__  двойными символами подчерка";
 
             var result = MarkdownProcessor.WrapEm(input);
 
-            Assert.AreEqual("Текст __окруженный с двух сторон__  двойными символами подчерка", result);
+            Assert.AreEqual(input, result);
         }
 
+        [Test]
+        public void not_wrap_text_between_two_escaped_underscores_to_em()
+        {
+            var input = @"Экранирование: \_Вот это\_, не должно выделиться тегом <em>";
+
+            var result = MarkdownProcessor.WrapEm(input);
+
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void not_wrap_text_between_double_underscore_and_single_underscore_to_em()
+        {
+            var input = @"Экранирование: __Вот это_, не должно выделиться тегом <em>";
+
+            var result = MarkdownProcessor.WrapEm(input);
+
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void not_wrap_text_between_single_underscore_and_double_underscore_to_em()
+        {
+            var input = @"Экранирование: _Вот это__, не должно выделиться тегом <em>";
+
+            var result = MarkdownProcessor.WrapEm(input);
+
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void wrap_text_on_two_lines_between_two_underscores_to_em()
+        {
+            var input = "Текст _окруженный \nсимволами_ подчеркивания в нескольких строках";
+
+            var result = MarkdownProcessor.WrapEm(input);
+
+            Assert.AreEqual("Текст <em>окруженный \nсимволами</em> подчеркивания в нескольких строках", result);
+        }
+
+        [Test]
+        public void not_wrap_double_underscores_to_em()
+        {
+            var input = "Текст с двойным __ подчеркиванием";
+
+            var result = MarkdownProcessor.WrapEm(input);
+
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void wrap_text_between_two_double_underscores_to_strong()
+        {
+            var input = "Текст с __двумя символами__ — д.б. жирным";
+
+            var result = MarkdownProcessor.WrapStrong(input);
+
+            Assert.AreEqual("Текст с <strong>двумя символами</strong> — д.б. жирным", result);
+        }
+
+        [Test]
+        public void wrap_text_between_first_two_double_underscores_to_strong()
+        {
+            var input = "Текст с __тремя__ двойными__ подчеркиваниями";
+
+            var result = MarkdownProcessor.WrapStrong(input);
+
+            Assert.AreEqual("Текст с <strong>тремя</strong> двойными__ подчеркиваниями", result);
+        }
+
+        [Test]
+        public void wrap_text_between_first_and_second_two_double_underscores_to_strong()
+        {
+            var input = "Текст __с __четырьмя__ двойными__ подчеркиваниями";
+
+            var result = MarkdownProcessor.WrapStrong(input);
+
+            Assert.AreEqual("Текст <strong>с </strong>четырьмя<strong> двойными</strong> подчеркиваниями", result);
+        }
+
+        [Test]
+        public void wrap_text_between_two_underscores_with_inner_double_underscore_to_em()
+        {
+            var input = "Внутри _выделения em может быть __ двойное подчеркивание_.";
+
+            var result = MarkdownProcessor.WrapEm(input);
+
+            Assert.AreEqual("Внутри <em>выделения em может быть __ двойное подчеркивание</em>.", result);
+        }
+
+        [Test]
+        public void wrap_text_between_two_underscores_with_inner_double_underscores_to_em()
+        {
+            var input = "Внутри _выделения em может быть __strong__ выделение_.";
+
+            var result = MarkdownProcessor.WrapEm(input);
+
+            Assert.AreEqual("Внутри <em>выделения em может быть __strong__ выделение</em>.", result);
+        }
+
+        [Test]
+        public void wrap_text_between_double_underscores_within_single_underscores_to_strong()
+        {
+            var input = "Внутри _выделения em может быть __strong__ выделение_.";
+
+            var result = MarkdownProcessor.WrapStrong(input);
+
+            Assert.AreEqual("Внутри _выделения em может быть <strong>strong</strong> выделение_.", result);
+        }
+
+        [Test]
+        public void wrap_text_between_double_underscores_with_inner_single_underscore_to_strong()
+        {
+            var input = "Внутри __выделения strong может быть _ одинарное__ подчеркивание.";
+
+            var result = MarkdownProcessor.WrapStrong(input);
+
+            Assert.AreEqual("Внутри <strong>выделения strong может быть _ одинарное</strong> подчеркивание.", result);
+        }
+
+        [Test]
+        public void wrap_text_between_double_underscores_with_inner_single_underscores_to_strong()
+        {
+            var input = "Внутри __выделения strong может быть _em_ выделение__.";
+
+            var result = MarkdownProcessor.WrapStrong(input);
+
+            Assert.AreEqual("Внутри <strong>выделения strong может быть _em_ выделение</strong>.", result);
+        }
+
+        [Test]
+        public void not_wrap_text_between_two_escaped_double_underscores_to_strong()
+        {
+            var input = @"Экранирование: \__Вот это\__, не должно выделиться тегом strong";
+
+            var result = MarkdownProcessor.WrapStrong(input);
+
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void not_wrap_text_between_double_underscore_and_triple_underscore_to_strong()
+        {
+            var input = @"Тройные подчеркивания: __Вот это___, не должно выделиться тегом strong";
+
+            var result = MarkdownProcessor.WrapStrong(input);
+
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void not_wrap_text_between_triple_underscore_and_double_underscore_to_strong()
+        {
+            var input = @"Тройные подчеркивания: ___Вот это__, не должно выделиться тегом strong";
+
+            var result = MarkdownProcessor.WrapStrong(input);
+
+            Assert.AreEqual(input, result);
+        }
     }
 
 
